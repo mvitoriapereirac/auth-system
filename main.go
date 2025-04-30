893dee384127363e835a45fb215fc5171116cacf
mvitoriapereirac <mvitoriapereirac@gmail.com>
2025-04-29 22:05:43 -0300

    restaura edicoes

--- main.go
+++ main.go
@@ -1,51 +1,70 @@
+// package main
+
+// import (
+//     "context"
+//     "fmt"
+//     "log"
+//     "net/http"
+
+//     "github.com/gin-gonic/gin"
+//     "github.com/go-redis/redis/v8"
+//     "gorm.io/driver/postgres"
+//     "gorm.io/gorm"
+// )
+
+// var (
+//     DB    *gorm.DB
+//     RDB   *redis.Client
+//     ctx   = context.Background()
+// )
+
+// func initPostgres() {
+//     dsn := "host=db user=postgres password=postgres dbname=auth_system port=5432 sslmode=disable"
+//     var err error
+//     DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+//     if err != nil {
+//         log.Fatal("Failed to connect to Postgres:", err)
+//     }
+//     fmt.Println("Connected to Postgres")
+// }
+
+// func initRedis() {
+//     RDB = redis.NewClient(&redis.Options{
+//         Addr: "redis:6379",
+//     })
+//     if _, err := RDB.Ping(ctx).Result(); err != nil {
+//         log.Fatal("Failed to connect to Redis:", err)
+//     }
+//     fmt.Println("Connected to Redis")
+// }
+
+// func main() {
+//     initPostgres()
+//     initRedis()
+
+//     r := gin.Default()
+//     r.GET("/ping", func(c *gin.Context) {
+//         c.JSON(http.StatusOK, gin.H{"message": "pong"})
+//     })
+
+//     r.Run(":8080")
+// }
+
 package main
 
 import (
-    "context"
-    "fmt"
-    "log"
-    "net/http"
-
-    "github.com/gin-gonic/gin"
-    "github.com/go-redis/redis/v8"
-    "gorm.io/driver/postgres"
-    "gorm.io/gorm"
+    "auth-system/app/config"
+    "auth-system/app/interfaces"
 )
-
-var (
-    DB    *gorm.DB
-    RDB   *redis.Client
-    ctx   = context.Background()
-)
-
-func initPostgres() {
-    dsn := "host=db user=postgres password=postgres dbname=auth_system port=5432 sslmode=disable"
-    var err error
-    DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-    if err != nil {
-        log.Fatal("Failed to connect to Postgres:", err)
-    }
-    fmt.Println("Connected to Postgres")
-}
-
-func initRedis() {
-    RDB = redis.NewClient(&redis.Options{
-        Addr: "redis:6379",
-    })
-    if _, err := RDB.Ping(ctx).Result(); err != nil {
-        log.Fatal("Failed to connect to Redis:", err)
-    }
-    fmt.Println("Connected to Redis")
-}
 
 func main() {
-    initPostgres()
-    initRedis()
+    dsn := "host=db user=postgres password=postgres dbname=auth_system port=5432 sslmode=disable"
 
-    r := gin.Default()
-    r.GET("/ping", func(c *gin.Context) {
-        c.JSON(http.StatusOK, gin.H{"message": "pong"})
-    })
+    db := config.InitDB(dsn)
+    redisClient := config.InitRedis()
 
-    r.Run(":8080")
+    server := interfaces.NewServer(db, redisClient)
+    server.Run(":8080")
+
+    
 }
